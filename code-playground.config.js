2fb310f70e261ed735c9c613944e675d20252ed4
Olivier Bossel <olivier.bossel@gmail.com>
2019-01-02 16:36:25 +0100

    update sitekey for demo

--- code-playground.config.js
+++ code-playground.config.js
@@ -21,20 +21,21 @@ module.exports = {
 		html : {
 			language : 'html',
 			data : `
-				<h1 class="h3 m-b-small">
-					Coffeekraken s-recaptcha-component
-				</h1>
-				<p class="p m-b-bigger">
-					Easily display a google recaptcha form element that will map on a simple checkbox for easy form validation. This does not replace the server side check of the captcha validation.
-				</p>
-				<form name="my-cool-form">
-					<input type="checkbox" name="my-captcha" style="display:none" required />
-					<s-recaptcha for="my-captcha" sitekey="6Ld4DmAUAAAAAGFPqTFuTAwhb9xClISGJStNPQR5"></s-recaptcha>
-					<s-validator for="my-captcha"></s-validator>
+				<div class="container">
+					<h1 class="h1 m-b-small">
+						Coffeekraken s-recaptcha-component
+					</h1>
+					<p class="p m-b-bigger">
+						Easily display a google recaptcha form element that will map on a simple checkbox for easy form validation. This does not replace the server side check of the captcha validation.
+					</p>
+					<form name="my-cool-form">
+						<input type="checkbox" name="my-captcha" style="display:none" required />
+						<s-recaptcha for="my-captcha" sitekey="6Lfx5TsUAAAAAMS8n1j_wV1meVwjpu161qhMpr3h"></s-recaptcha>
+						<s-validator for="my-captcha"></s-validator>
 
-					<input type="checkbox" name="my-captcha-2" style="display:none" required />
-					<s-recaptcha class="m-t" theme="dark" for="my-captcha-2" sitekey="6Ld4DmAUAAAAAGFPqTFuTAwhb9xClISGJStNPQR5"></s-recaptcha>
-					<s-validator for="my-captcha-2"></s-validator>
+						<input type="checkbox" name="my-captcha-2" style="display:none" required />
+						<s-recaptcha class="m-t" theme="dark" for="my-captcha-2" sitekey="6Lfx5TsUAAAAAMS8n1j_wV1meVwjpu161qhMpr3h"></s-recaptcha>
+						<s-validator for="my-captcha-2"></s-validator>
 
 					<input type="submit" value="Submit" class="btn btn--primary m-t" />
 				</form>
